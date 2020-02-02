13ed0dfd52dbc1f43eabf056e55e85e0941bd819
Liu Zhenhua <join_github_repo@126.com>
2020-02-02 16:22:24 +0800

    04 FakeHead LinkedList

---
+++ 04-LinkedList/03-FakeNode-LinkedList/src/MainApp.java
@@ -0,0 +1,12 @@
+public class MainApp{
+	public static void main(String[] args){
+		
+		LinkedList<Integer> list = new LinkedList<>();
+
+		list.addFirst(18);
+		list.addFirst(89);
+		list.addFirst(100);
+		list.addFirst(200);
+
+	}
+}
