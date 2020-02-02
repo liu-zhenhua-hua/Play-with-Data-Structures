593af3c26d7afecb9c8409329910d3e261b2f5de
Liu Zhenhua <join_github_repo@126.com>
2020-02-02 20:19:22 +0800

    04 Query and Update LinkedList

--- 04-LinkedList/04-Query-and-Update-LinkedList/src/LinkedList.java
+++ 04-LinkedList/04-Query-and-Update-LinkedList/src/LinkedList.java
@@ -2,5 +2,76 @@
 * @Author: Tony Liu
 * @Date:   2020-02-02 20:04:12
 * @Modified by:   Tony Liu
-* @Last Modified time: 2020-02-02 20:04:32
+* @Last Modified time: 2020-02-02 20:18:16
 */
+
+public class LinkedList<E>{
+
+
+	private class Node{
+		public E e;
+		public Node next;
+
+
+		public Node(E e, Node next){
+			this.e = e;
+			this.next = next;
+		}
+
+		public Node(E e){
+			this(e,null);
+		}
+
+
+		public Node(){
+			this(null,null);
+		}
+
+
+		@Override
+		public String toString(){
+			return e.toString();
+		}
+
+	}
+
+
+	private Node fakeHead;
+	private int size;
+
+
+
+	public LinkedList(){
+		fakeHead = new Node();
+		size = 0;
+	}
+
+
+	public int getSize(){
+		return size;
+	}
+
+
+	public boolean isEmpty(){
+		return size == 0;
+	}
+
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
