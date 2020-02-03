0540b4087d24e7cba8c7bf7f30590473a87f87e0
Liu Zhenhua <join_github_repo@126.com>
2020-02-03 19:18:37 +0800

    03 Stack and Queue

--- 03-Stack-Queue/01-ArrayList-Stack/src/ArrayStackImpl.java
+++ 03-Stack-Queue/01-ArrayList-Stack/src/ArrayStackImpl.java
@@ -2,7 +2,7 @@
 * @Author: Tony Liu
 * @Date:   2020-02-03 17:35:33
 * @Modified by:   Tony Liu
-* @Last Modified time: 2020-02-03 17:50:41
+* @Last Modified time: 2020-02-03 18:00:31
 */
 
 
@@ -36,7 +36,33 @@ public class ArrayStackImpl<E> implements Stack<E>{
 	}
 
 
+	@Override
+	public void push(E e){
+		array.addLast(e);
+	}
+
+
+	@Override
+	public E pop(){
+		return array.deleteLast();
+	}
+
+	@Override
+	public E peek(){
+		return array.getLastElement();
+	}
+
+
 	
+
+
+
+
+
+
+
+
+
 
 
 }
