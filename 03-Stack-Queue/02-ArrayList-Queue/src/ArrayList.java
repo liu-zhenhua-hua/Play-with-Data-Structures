32b4f7d4bc0c9d948190bdc3f13d51f8f93b5835
Liu Zhenhua <join_github_repo@126.com>
2020-02-04 11:04:44 +0800

    03 ArrayList Queue

---
+++ 03-Stack-Queue/02-ArrayList-Queue/src/ArrayList.java
@@ -0,0 +1,78 @@
+/*
+* @Author: Tony Liu
+* @Date:   2020-02-04 10:44:15
+* @Modified by:   Tony Liu
+* @Last Modified time: 2020-02-04 11:03:22
+*
+*
+* Queue based ArrayList
+*/
+
+
+public class ArrayList<E>{
+
+	private E[] elementContainer;
+
+	private int size;
+
+
+	public ArrayList(int capacity){
+		elementContainer = (E[]) new Object[capacity];
+		size = 0;
+	}
+
+
+	public ArrayList(){
+		this(15);
+	}
+
+
+	public int getSize(){
+		return size;
+	}
+
+
+	public int getCapacity(){
+		return elementContainer.length;
+	}
+
+	public boolean isEmpty(){
+		return size == 0;
+	}
+
+
+	public void add(int index, E e){
+		if(index < 0 || index > size)
+			throw new IllegalArgumentException("Invalid Argument, please try again !");
+
+		if(size == elementContainer.length)
+			throw new IllegalArgumentException("The ArrayList is Full, Adding element is Failed !");
+
+		for(int i=size-1;i>=index;i--){
+			elementContainer[i+1] = elementContainer[i];
+		}
+
+		elementContainer[index] = e;
+		size++;
+
+	}
+
+
+
+
+	@Override
+	public String toString(){
+		StringBuilder result = new StringBuilder();
+		result.append(String.format("ArrayList's Size : %d, ArrayList's Capapcity : %d\n",size,elementContainer.length));
+		result.append('[');
+		for(int i =0;i<size;i++){
+			result.append(elementContainer[i]);
+			if( i != size-1)
+				result.append(",");
+		}
+		result.append(']');
+		return result.toString();
+	}
+
+
+}
