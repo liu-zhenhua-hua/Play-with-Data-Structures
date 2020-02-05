9fb4719eda10900ab0d7a7fb7444bc0faff1fd4f
Liu Zhenhua <join_github_repo@126.com>
2020-02-05 22:33:36 +0800

    05 LinkedList Recursion

---
+++ 05-Linked-Recursion/02-Testing-LeetCode-Solution/src/Solution1.java
@@ -0,0 +1,41 @@
+/*
+* @Author: Tony Liu
+* @Date:   2020-02-05 22:23:46
+* @Last Modified by:   Liu Zhenhua
+* @Last Modified time: 2020-02-05 22:33:11
+*/
+
+
+public class Solution1{
+
+	public ListNode removeElements(ListNode head, int val){
+
+		while(head != null && head.val == val){
+			ListNode remNode = head;
+			head = head.next;
+			remNode.next = null;
+		}
+
+
+		if(head == null){
+			return head;
+		}
+
+		ListNode prev = head;
+		while(prev.next != null){
+			if(prev.next.val == val){
+				ListNode remNode = prev.next;
+				prev.next = remNode.next;
+				remNode.next = null;
+			}else{
+				prev = prev.next;
+			}
+
+
+		}
+		return head;
+
+	}
+
+
+}
