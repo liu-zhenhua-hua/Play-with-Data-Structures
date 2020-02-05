71eb0adb36874ce3a55d73b7af0b9a1438c97a1e
Liu Zhenhua <join_github_repo@126.com>
2020-02-05 15:05:28 +0800

    05 LinkedList Recursion ListNode

---
+++ 05-Linked-Recursion/01-LeetCode-Problems-203/src/ListNode.java
@@ -0,0 +1,16 @@
+/*
+* @Author: Tony Liu
+* @Date:   2020-02-05 15:01:33
+* @Modified by:   Tony Liu
+* @Last Modified time: 2020-02-05 15:02:44
+*/
+public class ListNode{
+
+	int val;
+	ListNode next;
+
+	public ListNode(int x){
+		val = x;
+	}
+
+}
