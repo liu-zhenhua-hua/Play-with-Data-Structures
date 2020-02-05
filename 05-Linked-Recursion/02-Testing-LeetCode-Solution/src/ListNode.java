e2d5c58653f30278b87303bae000c81e6cb8e1c7
Liu Zhenhua <join_github_repo@126.com>
2020-02-05 22:12:44 +0800

    05 LinkedList Recursion

--- 05-Linked-Recursion/02-Testing-LeetCode-Solution/src/ListNode.java
+++ 05-Linked-Recursion/02-Testing-LeetCode-Solution/src/ListNode.java
@@ -2,7 +2,7 @@
 * @Author: Tony Liu
 * @Date:   2020-02-05 22:04:26
 * @Modified by:   Tony Liu
-* @Last Modified time: 2020-02-05 22:06:32
+* @Last Modified time: 2020-02-05 22:12:24
 */
 
 public class ListNode{
@@ -16,7 +16,19 @@ public class ListNode{
 	}
 
 
-	
+	public ListNode(int[] array){
+		if(array == null || array.length == 0)
+			throw new IllegalArgumentException("Array can not be empty !");
+
+		this.val = array[0];
+		ListNode cur = this;
+
+		for(int i=0;i<array.length;i++){
+			cur.next = new ListNode(array[i]);
+			cur = cur.next;
+		}
+
+	}
 
 
 
