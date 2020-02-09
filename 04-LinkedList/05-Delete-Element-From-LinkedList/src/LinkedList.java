86169ec537ca21fdcfdaa56a6d73532396b1ea40
Liu Zhenhua <join_github_repo@126.com>
2020-02-09 12:19:20 +0800

    04 LinkedList

--- 04-LinkedList/05-Delete-Element-From-LinkedList/src/LinkedList.java
+++ 04-LinkedList/05-Delete-Element-From-LinkedList/src/LinkedList.java
@@ -2,7 +2,7 @@
 * @Author: Tony Liu
 * @Date:   2020-02-02 20:58:44
 * @Modified by:   Tony Liu
-* @Last Modified time: 2020-02-02 21:01:16
+* @Last Modified time: 2020-02-09 12:12:09
 *
 * Removed the Element from Linked List
 *
@@ -36,5 +36,150 @@ public class LinkedList<E>{
 
 	}
 
+	private Node dummyHead;
+    private int size;
+
+    public LinkedList(){
+        dummyHead = new Node();
+        size = 0;
+    }
+
+    
+    public int getSize(){
+        return size;
+    }
+
+    
+    public boolean isEmpty(){
+        return size == 0;
+    }
+
+    
+    public void add(int index, E e){
+
+        if(index < 0 || index > size)
+            throw new IllegalArgumentException("Add failed. Illegal index.");
+
+        Node prev = dummyHead;
+        for(int i = 0 ; i < index ; i ++)
+            prev = prev.next;
+
+        prev.next = new Node(e, prev.next);
+        size ++;
+    }
+
+    
+    public void addFirst(E e){
+        add(0, e);
+    }
+
+    
+    public void addLast(E e){
+        add(size, e);
+    }
+
+    
+    public E get(int index){
+
+        if(index < 0 || index >= size)
+            throw new IllegalArgumentException("Get failed. Illegal index.");
+
+        Node cur = dummyHead.next;
+        for(int i = 0 ; i < index ; i ++)
+            cur = cur.next;
+        return cur.e;
+    }
+
+    
+    public E getFirst(){
+        return get(0);
+    }
+
+    
+    public E getLast(){
+        return get(size - 1);
+    }
+
+    
+    public void set(int index, E e){
+        if(index < 0 || index >= size)
+            throw new IllegalArgumentException("Set failed. Illegal index.");
+
+        Node cur = dummyHead.next;
+        for(int i = 0 ; i < index ; i ++)
+            cur = cur.next;
+        cur.e = e;
+    }
+
+    
+    public boolean contains(E e){
+        Node cur = dummyHead.next;
+        while(cur != null){
+            if(cur.e.equals(e))
+                return true;
+            cur = cur.next;
+        }
+        return false;
+    }
+
+    
+    public E remove(int index){
+        if(index < 0 || index >= size)
+            throw new IllegalArgumentException("Remove failed. Index is illegal.");
+
+        Node prev = dummyHead;
+        for(int i = 0 ; i < index ; i ++)
+            prev = prev.next;
+
+        Node retNode = prev.next;
+        prev.next = retNode.next;
+        retNode.next = null;
+        size --;
+
+        return retNode.e;
+    }
+
+    
+    public E removeFirst(){
+        return remove(0);
+    }
+
+    
+    public E removeLast(){
+        return remove(size - 1);
+    }
+
+    
+    public void removeElement(E e){
+
+        Node prev = dummyHead;
+        while(prev.next != null){
+            if(prev.next.e.equals(e))
+                break;
+            prev = prev.next;
+        }
+
+        if(prev.next != null){
+            Node delNode = prev.next;
+            prev.next = delNode.next;
+            delNode.next = null;
+            size --;
+        }
+    }
+
+    @Override
+    public String toString(){
+        StringBuilder res = new StringBuilder();
+
+        Node cur = dummyHead.next;
+        while(cur != null){
+            res.append(cur + "->");
+            cur = cur.next;
+        }
+        res.append("NULL");
+
+        return res.toString();
+    }
+	
 
 }
