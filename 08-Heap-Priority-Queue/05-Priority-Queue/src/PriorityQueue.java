f911eab125355a12b270bf7ab2b396b1b24d142b
Liu Zhenhua <join_github_repo@126.com>
2020-02-12 21:28:40 +0800

    08 Heap and Queue

--- 08-Heap-Priority-Queue/05-Priority-Queue/src/PriorityQueue.java
+++ 08-Heap-Priority-Queue/05-Priority-Queue/src/PriorityQueue.java
@@ -2,9 +2,14 @@
 * @Author: Tony Liu
 * @Date:   2020-02-12 21:24:38
 * @Modified by:   Tony Liu
-* @Last Modified time: 2020-02-12 21:25:36
+* @Last Modified time: 2020-02-12 21:28:21
 */
 
 public class PriorityQueue<E extends Comparable<E>> implements Queue<E>{
-	
+
+	private MaxHeap<E> maxHeap;
+
+	public PriorityQueue(){
+		maxHeap = new MaxHeap<>();
+	}
 }
