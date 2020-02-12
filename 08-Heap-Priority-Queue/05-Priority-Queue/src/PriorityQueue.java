57a312ffdffb1b005c47301ed2f7a27ea0d78117
Liu Zhenhua <join_github_repo@126.com>
2020-02-12 21:25:44 +0800

    08 Heap and Priority Queue

---
+++ 08-Heap-Priority-Queue/05-Priority-Queue/src/PriorityQueue.java
@@ -0,0 +1,10 @@
+/*
+* @Author: Tony Liu
+* @Date:   2020-02-12 21:24:38
+* @Modified by:   Tony Liu
+* @Last Modified time: 2020-02-12 21:25:36
+*/
+
+public class PriorityQueue<E extends Comparable<E>> implements Queue<E>{
+	
+}
