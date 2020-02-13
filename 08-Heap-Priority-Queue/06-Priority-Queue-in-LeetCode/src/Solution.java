520199490bce490799f6558b29b70a6bbe7de09e
Liu Zhenhua <join_github_repo@126.com>
2020-02-13 14:46:48 +0800

    08 Heap and Queue

--- 08-Heap-Priority-Queue/06-Priority-Queue-in-LeetCode/src/Solution.java
+++ 08-Heap-Priority-Queue/06-Priority-Queue-in-LeetCode/src/Solution.java
@@ -1,10 +1,19 @@
 /*
   Solution LeetCode #347 
 */
+import java.util.List;
+import java.util.TreeMap;
+
 public class Solution{
 
 
 	public List<Integer> topKFrequent(int[] nums, int k){
-		
+		TreeMap<Integer, Integer> map = new TreeMap<>();
+		for(int num: nums){
+			if(map.containsKey(num))
+				map.put(num,map.get(num)+1);
+			else
+				map.put(num,1);
+		}
 	}
 }
