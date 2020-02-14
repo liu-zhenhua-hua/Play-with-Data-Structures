2a957ea0bc0bffe96b03a11a2fce3d46f89352f3
Liu Zhenhua <join_github_repo@126.com>
2020-02-14 22:57:05 +0800

    13 Red Black Tree

--- 13-Red-Black-Tree/01-Equivalence-of-RBTree-and-2-3-Tree/src/RBTree.java
+++ 13-Red-Black-Tree/01-Equivalence-of-RBTree-and-2-3-Tree/src/RBTree.java
@@ -2,11 +2,15 @@
 * @Author: Tony Liu
 * @Date:   2020-02-14 22:50:22
 * @Modified by:   Tony Liu
-* @Last Modified time: 2020-02-14 22:51:21
+* @Last Modified time: 2020-02-14 22:56:58
 */
+import java.util.ArrayList;
+
 
 public class RBTree<K extends Comparable<k>, V>{
 
+	private static final boolean RED = true;
+	private static final boolean BLACK = false;
 
-	
+
 }
